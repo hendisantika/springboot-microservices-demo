01a5997deac31494efbd6907a17dc819f337a347
hendisantika <hendisantika@yahoo.co.id>
2019-07-28 15:25:45 +0700

    #9 - Enable Discovery Client in Profile Microservice Producer

--- profile-microservice-producer/src/main/java/com/hendisantika/microservices/profilemicroserviceproducer/ProfileMicroserviceProducerApplication.java
+++ profile-microservice-producer/src/main/java/com/hendisantika/microservices/profilemicroserviceproducer/ProfileMicroserviceProducerApplication.java
@@ -2,8 +2,10 @@ package com.hendisantika.microservices.profilemicroserviceproducer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ProfileMicroserviceProducerApplication {
 
     public static void main(String[] args) {
