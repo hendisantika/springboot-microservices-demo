1d2e7902a7f4608bc31a031dfa38f70468884f3b
hendisantika <hendisantika@yahoo.co.id>
2019-07-28 15:26:26 +0700

    #10 - Add ProfileRepository in Profile Microservice Producer

---
+++ profile-microservice-producer/src/main/java/com/hendisantika/microservices/profilemicroserviceproducer/ProfileRepository.java
@@ -0,0 +1,18 @@
+package com.hendisantika.microservices.profilemicroserviceproducer;
+
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : profile-microservice-producer
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-07-28
+ * Time: 15:25
+ */
+public interface ProfileRepository {
+    List<Profile> getAllProfiles();
+
+    Profile getProfile(String userId);
+}
