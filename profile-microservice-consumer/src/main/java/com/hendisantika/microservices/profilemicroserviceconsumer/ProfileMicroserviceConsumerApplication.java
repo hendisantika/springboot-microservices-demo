1ef46243f4d5d10e04417e75fc4282cae3407b88
hendisantika <hendisantika@yahoo.co.id>
2019-07-28 15:32:08 +0700

    #14 - Add Profile Microservice Consumer Module

---
+++ profile-microservice-consumer/src/main/java/com/hendisantika/microservices/profilemicroserviceconsumer/ProfileMicroserviceConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.microservices.profilemicroserviceconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfileMicroserviceConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProfileMicroserviceConsumerApplication.class, args);
+    }
+
+}
---
+++ profile-microservice-consumer/src/test/java/com/hendisantika/microservices/profilemicroserviceconsumer/ProfileMicroserviceConsumerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hendisantika.microservices.profilemicroserviceconsumer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProfileMicroserviceConsumerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
