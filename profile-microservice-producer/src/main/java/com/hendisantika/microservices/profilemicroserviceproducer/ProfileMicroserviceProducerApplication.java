b009aef0b1367fe42e2310af79d37c94e6e88188
hendisantika <hendisantika@yahoo.co.id>
2019-07-28 15:22:40 +0700

    #5 - Add Profile Microservice Producer

---
+++ profile-microservice-producer/src/main/java/com/hendisantika/microservices/profilemicroserviceproducer/ProfileMicroserviceProducerApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.microservices.profilemicroserviceproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfileMicroserviceProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProfileMicroserviceProducerApplication.class, args);
+    }
+
+}
